1aa9ddef5f8265f0ef6b0dfc2d6ca9ea947b9256
Thomas Kiley <thomas.kiley@diffblue.com>
2020-01-31 22:25:28 +0000

    Delete loads of redundant code

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public enum RotationAxes
-    {
-        MouseXAndY = 0,
-        MouseX = 1,
-        MouseY = 2
-    }
-
-    public RotationAxes axes = RotationAxes.MouseXAndY;
     public float sensitivityX = 15F;
     public float sensitivityY = 15F;
     public float minimumX = -360F;
@@ -20,60 +12,22 @@ public class CameraController : MonoBehaviour
     public float maximumY = 60F;
     float rotationX = 0F;
     float rotationY = 0F;
-    private List<float> rotArrayX = new List<float>();
-    float rotAverageX = 0F;
-    private List<float> rotArrayY = new List<float>();
-    float rotAverageY = 0F;
     public float frameCounter = 20;
     Quaternion originalRotation;
 
     void Update()
     {
-        //Resets the average rotation
-        rotAverageY = 0f;
-        rotAverageX = 0f;
-
         //Gets rotational input from the mouse
         rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
         rotationX += Input.GetAxis("Mouse X") * sensitivityX;
 
-        //Adds the rotation values to their relative array
-        rotArrayY.Add(rotationY);
-        rotArrayX.Add(rotationX);
-
-        //If the arrays length is bigger or equal to the value of frameCounter remove the first value in the array
-        if (rotArrayY.Count >= frameCounter)
-        {
-            rotArrayY.RemoveAt(0);
-        }
-
-        if (rotArrayX.Count >= frameCounter)
-        {
-            rotArrayX.RemoveAt(0);
-        }
-
-        //Adding up all the rotational input values from each array
-        for (int j = 0; j < rotArrayY.Count; j++)
-        {
-            rotAverageY += rotArrayY[j];
-        }
-
-        for (int i = 0; i < rotArrayX.Count; i++)
-        {
-            rotAverageX += rotArrayX[i];
-        }
-
-        //Standard maths to find the average
-        rotAverageY /= rotArrayY.Count;
-        rotAverageX /= rotArrayX.Count;
-
         //Clamp the rotation average to be within a specific value range
-        rotAverageY = ClampAngle(rotAverageY, minimumY, maximumY);
-        rotAverageX = ClampAngle(rotAverageX, minimumX, maximumX);
+        float clampedRotationY = ClampAngle(rotationY, minimumY, maximumY);
+        float clampedRotationX = ClampAngle(rotationX, minimumX, maximumX);
 
         //Get the rotation you will be at next as a Quaternion
-        Quaternion yQuaternion = Quaternion.AngleAxis(rotAverageY, Vector3.left);
-        Quaternion xQuaternion = Quaternion.AngleAxis(rotAverageX, Vector3.up);
+        Quaternion yQuaternion = Quaternion.AngleAxis(clampedRotationY, Vector3.left);
+        Quaternion xQuaternion = Quaternion.AngleAxis(clampedRotationX, Vector3.up);
 
         //Rotate
         transform.localRotation = originalRotation * xQuaternion * yQuaternion;
@@ -84,7 +38,7 @@ public class CameraController : MonoBehaviour
         originalRotation = transform.localRotation;
     }
 
-    public static float ClampAngle(float angle, float min, float max)
+    static float ClampAngle(float angle, float min, float max)
     {
         angle = angle % 360;
         if ((angle >= -360F) && (angle <= 360F))
