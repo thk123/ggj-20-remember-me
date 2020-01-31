0add1f5a710bfaf32f4ce6d6a3ce812a2b6d3205
adamb <a.bebars@sae.edu>
2020-01-31 23:43:21 +0000

    Re-added dissapear

--- Assets/Scripts/Dissapear.cs
+++ Assets/Scripts/Dissapear.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dissapear : MonoBehaviour
+public class DissapearOncontact : MonoBehaviour
 {
+    public KeyCode AlphaUp;
+    public KeyCode AlphaDown;
+    public float Aalpha = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var material = GetComponent<Renderer>().material;
+        var color = material.color;
+        if (Input.GetKeyDown(AlphaUp)) { material.color = new Color(1, 0, 0, 1); }
+        if (Input.GetKeyDown(AlphaDown)) { material.color = new Color(255, 255, 255, 1); }
     }
 }
