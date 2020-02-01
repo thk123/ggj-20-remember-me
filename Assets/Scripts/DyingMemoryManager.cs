aef8c6df34b265552405405e1e0cf83def486683
Thomas Kiley <thomas.kiley@diffblue.com>
2020-02-01 11:47:02 +0000

    Add in the fading memory stuff

---
+++ Assets/Scripts/DyingMemoryManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DyingMemoryManager : MonoBehaviour
+{
+    private DyingMemory[] dyingMemories;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dyingMemories = FindObjectsOfType<DyingMemory>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dyingMemories.All(memory => memory.GetComponent<Dissapear>().IsVisbile))
+        {
+            Debug.Log("All memories visible!!");    
+        }
+        
+    }
+}
