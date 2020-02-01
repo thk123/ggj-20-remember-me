302b1afea6452e7f2d5c154ffd0f67c8a61db6f9
adamb <a.bebars@sae.edu>
2020-02-01 00:32:11 +0000

    Partial work on trees appearing

---
+++ Assets/Scripts/TreeAppear.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeAppear : MonoBehaviour
+{
+    public bool Visible = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //var TreesObjects = GameObject.FindGameObjectsWithTag("Tree");
+        //for (Tree in TreesObjects) Tree.GetComponent(TreeAppear).Visible = true;
+
+        var material = GetComponent<Renderer>().material;
+        var color = material.color;
+        if (Visible == true) { material.color = new Color(0, 0, 1, 1); }
+        if (Visible == false) { material.color = new Color(255, 255, 255, 1); }
+    }
+
+}
