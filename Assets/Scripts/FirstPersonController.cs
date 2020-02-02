8210a1ed834e705ad2ee9e13b8078f3852ea19e9
Thomas Kiley <thomas.kiley@diffblue.com>
2020-02-02 11:13:47 +0000

    Remove noisy logging

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -47,8 +47,6 @@ public class FirstPersonController : MonoBehaviour
             {
                 lastPlayedSound = Time.time + soundPlayInterval;
                 audioPlayer.playAudio();
-                print("Play");
-
             }
         }
     }
