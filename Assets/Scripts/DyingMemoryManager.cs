c6ff109f9b54c7eeec1ab70ae3dfa4b19b5d3721
Thomas Kiley <thomas.kiley@diffblue.com>
2020-02-02 03:24:49 +0000

    Adding audio chimes
    A

--- Assets/Scripts/DyingMemoryManager.cs
+++ Assets/Scripts/DyingMemoryManager.cs
@@ -7,12 +7,33 @@ public class DyingMemoryManager : MonoBehaviour
 {
     private DyingMemory[] dyingMemories;
 
-    public Dialogue ThingToEnable; 
+    public Dialogue ThingToEnable;
+
+    public AudioClip[] successChimes;
 
     // Start is called before the first frame update
     void Start()
     {
         dyingMemories = FindObjectsOfType<DyingMemory>();
+        StartCoroutine(chimes());
+    }
+
+    IEnumerator chimes()
+    {
+        int chimesPlayed = 0;
+        int totalDyingMemories = getTotalDyingMemories();
+        if (totalDyingMemories > successChimes.Length)
+        {
+            Debug.LogWarning("Insufficent chimes");
+        }
+        while (getDyingMemoriesComplete() < totalDyingMemories)
+        {
+            yield return new WaitUntil(() => getDyingMemoriesComplete() != chimesPlayed);
+            Debug.Log("chime! " + getTotalDyingMemories());
+            GetComponent<AudioSource>().PlayOneShot(successChimes[successChimes.Length - totalDyingMemories + chimesPlayed]);
+            ++chimesPlayed;
+        }
+        
     }
 
     // Update is called once per frame
@@ -35,5 +56,14 @@ public class DyingMemoryManager : MonoBehaviour
             }
         }
         
+    }
+    int getDyingMemoriesComplete()
+    {
+        return dyingMemories.Count(memory => memory.GetComponent<Dissapear>().Tauched);
+    }
+    
+    int getTotalDyingMemories()
+    {
+        return dyingMemories.Length;
     }
 }
